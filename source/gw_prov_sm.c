5695e95cb308da18bae90116a7a1f259b446a68e
apattu <alagupandi_pattu@comcast.com>
2019-07-24 18:44:19 +0000

    TCCBR-3982 : Xfinity wifi ssid not created/broadcasted
    Reason for change: Currently we are staring hotspot-start event
    from both cosa-start-rem.sh and also in gwprov app. so we ended
    up in setting in hotspot status as started. leading to the issues.
    moved to appropriate places
    Test Procedure:
    1. check the mentioned scenario in the bug works fine and after reboot
       as well.
    Risks: Low
    Signed-off-by:alagupandi_pattu@comcast.com
    
    (cherry picked from commit e8d1a6839eb8b0fef631ed29730c796e43b93fbe)
    
    Change-Id: I278aa90308937ad37077e0b366f44d087ca6f6c5

--- source/gw_prov_sm.c
+++ source/gw_prov_sm.c
@@ -1647,8 +1647,9 @@ static void *GWP_sysevent_threadfunc(void *data)
 #endif
                    
                     if (!hotspot_started) {
-#if defined(INTEL_PUMA7) || defined(_COSA_BCM_MIPS_) || defined(_COSA_BCM_ARM_) && !defined(_CBR_PRODUCT_REQ_)
-                        printf("Not Calling hotspot-start for XB3 and XB6 it will be done in cosa_start_rem.sh and hotspot.service respectively\n");
+#if defined(INTEL_PUMA7) || defined(_COSA_BCM_MIPS_) || defined(_COSA_BCM_ARM_)
+                        printf("Not Calling hotspot-start for XB3,XB6 and CBR it will be done in \
+				cosa_start_rem.sh,hotspot.service and xfinity_hotspot_bridge_setup.sh respectively\n");
 #else
                         sysevent_set(sysevent_fd_gs, sysevent_token_gs, "hotspot-start", "", 0);
                         hotspot_started = 1 ;
