406fed874626fc17033608683eacc58eb14f7bdd
Andre McCurdy <armccurdy@gmail.com>
2020-07-02 23:41:46 -0700

    RDKCOM-2510: RDKBDEV-656: avoid unnecessary duplication in definition
    Reason for change:
    avoid unnecessary duplication in definition of struct __appCallBack
    Don't duplicate the entire structure to conditionally add one element
    to it.
    Test Procedure: Sanity.
    Risks: None.
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    (cherry picked from commit a2563d6a68f2ab9532d8c25df25a82478cd7cdcb)
    Change-Id: I165ab911c188ba5ddd189465816310a6207124af

--- source/include/gw_prov_abstraction.h
+++ source/include/gw_prov_abstraction.h
@@ -232,7 +232,6 @@ typedef void (*fpGW_SetTopologyMode)(Uint8 type, Uint16 length, const Uint8* val
 /*! \var typedef struct __appCallBack 
 *   \brief struct of pointers to the function pointers of callback functions.
 */
-#ifdef CISCO_CONFIG_DHCPV6_PREFIX_DELEGATION
 typedef struct __appCallBack
 {
 	fpDocsisLinkDown_1 pGWP_act_DocsisLinkDown_1;
@@ -245,23 +244,11 @@ typedef struct __appCallBack
 	fpProvEntry pGWP_act_ProvEntry;
 	fpDocsisEnabled pDocsis_gotEnable;
 	fpGW_Tr069PaSubTLVParse pGW_Tr069PaSubTLVParse;
+#ifdef CISCO_CONFIG_DHCPV6_PREFIX_DELEGATION
 	fpGW_SetTopologyMode pGW_SetTopologyMode;
-}appCallBack;
-#else
-typedef struct __appCallBack
-{
-	fpDocsisLinkDown_1 pGWP_act_DocsisLinkDown_1;
-        fpDocsisLinkDown_2 pGWP_act_DocsisLinkDown_2;
-	fpDocsisLinkUp pGWP_act_DocsisLinkUp;
-	fpDocsisCfgfile pGWP_act_DocsisCfgfile;
-	fpDocsisTftpOk pGWP_act_DocsisTftpOk;
-	fpBefCfgfileEntry pGWP_act_BefCfgfileEntry;
-	fpDocsisInited pGWP_act_DocsisInited;
-	fpProvEntry pGWP_act_ProvEntry;
-        fpDocsisEnabled pDocsis_gotEnable;
-        fpGW_Tr069PaSubTLVParse pGW_Tr069PaSubTLVParse;
-}appCallBack;
 #endif
+}appCallBack;
+
  /*
  * Function Definitions
  */
