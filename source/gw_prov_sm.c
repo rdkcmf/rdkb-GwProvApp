8686fc656616ad18ee8e972889c8008f4c813a3f
rajakumaran.a <rajakumaran.a@ltts.com>
2020-10-19 14:26:32 +0000

    BD-877: Compliance Issue - Code Match in GwProvApp
    Reason for change:  Compliance issue
    Test procedure: build and test
    Risks: Low
    
    Signed-off-by: rajakumaran.a <rajakumaran.a@ltts.com>
    Change-Id: I233b016a6667cd3e26cfb638d65958202c11ed13

--- source/gw_prov_sm.c
+++ source/gw_prov_sm.c
@@ -1645,39 +1645,36 @@ static void check_lan_wan_ready()
 **************************************************************************/
 static void *GWP_lxcserver_threadfunc(void *data)
 {
-    int server_fd, new_socket, valread;
+    int fd_ser, new_socket, valread;
     struct sockaddr_in address;
-    int opt = 1;
+    int option = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
     char *token = NULL;
 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    if ((fd_ser = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
+    if (setsockopt(fd_ser, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &option, sizeof(option)))
     {
-        perror("setsockopt");
+        perror("setsockoption");
         exit(EXIT_FAILURE);
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
 
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address,
+    if (bind(fd_ser, (struct sockaddr *)&address,
                                  sizeof(address))<0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0)
+    if (listen(fd_ser, 3) < 0)
     {
         perror("listen");
         exit(EXIT_FAILURE);
@@ -1685,7 +1682,7 @@ static void *GWP_lxcserver_threadfunc(void *data)
     // To keep listening for client (pandm) connection
     while(1)
     {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+        if ((new_socket = accept(fd_ser, (struct sockaddr *)&address,
                        (socklen_t*)&addrlen))<0)
         {
             perror("accept");
