b5b83dbebdea4d0a8dad201bfca118c19a7bc9c5
rajakumaran.a <rajakumaran.a@ltts.com>
2020-10-19 14:26:32 +0000

    RDKCOM-1654: BD-877: lxc-server code modifications in GwProvApp for RPI
    Reason for change: Gwprovapp lxc-server code enhancements
    Test procedure: build and test
    Risks: Low
    Signed-off-by: rajakumaran.a <rajakumaran.a@ltts.com>
    Test Procedure: None.
    (cherry picked from commit 45c2c48a185c3686376a879a7b90039ef485a8f9)
    Change-Id: I1b3e80acdd162a21af9bbd1eb9160d1bb2121c68

--- source/gw_prov_sm.c
+++ source/gw_prov_sm.c
@@ -1650,50 +1650,42 @@ static void check_lan_wan_ready()
 **************************************************************************/
 static void *GWP_lxcserver_threadfunc(void *data)
 {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
+//lxc server to to listen for pandm client 
+    int lxcsock_fd, lxcconn_fd, readbytes;
+    struct sockaddr_in lxcsock_addr;
+    int lxcsock_opt = 1;
+    int addrlen = sizeof(lxcsock_addr);
     char buffer[1024] = {0};
     char *token = NULL;
-
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    if((lxcsock_fd = socket(AF_INET, SOCK_STREAM, 0))==0)
     {
-        perror("socket failed");
+        perror("lxc server socket creation failed");
         exit(EXIT_FAILURE);
     }
-
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
+    if (setsockopt(lxcsock_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &lxcsock_opt, sizeof(lxcsock_opt)))
     {
-        perror("setsockopt");
+        perror("lxc server set socket option failed");
         exit(EXIT_FAILURE);
     }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address,
-                                 sizeof(address))<0)
+    lxcsock_addr.sin_family = AF_INET;
+    lxcsock_addr.sin_addr.s_addr = INADDR_ANY;
+    lxcsock_addr.sin_port = htons( PORT );
+    if (bind(lxcsock_fd, (struct sockaddr *)&lxcsock_addr, sizeof(lxcsock_addr))<0)
     {
-        perror("bind failed");
+        perror("lxc server bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0)
+    if (listen(lxcsock_fd, 3) < 0)
     {
-        perror("listen");
+        perror("lxc server listen failed");
         exit(EXIT_FAILURE);
     }
     // To keep listening for client (pandm) connection
     while(1)
     {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                       (socklen_t*)&addrlen))<0)
+        if ((lxcconn_fd = accept(lxcsock_fd, (struct sockaddr *)&lxcsock_addr, (socklen_t*)&addrlen))<0)
         {
-            perror("accept");
+            perror("lxc server accept failed");
             exit(EXIT_FAILURE);
         }
        else
@@ -1702,10 +1694,11 @@ static void *GWP_lxcserver_threadfunc(void *data)
     // To keep listening for sys event message from pandm client
     while(1)
     {
-        valread = recv( new_socket , buffer, 1024,0);
+        readbytes = recv( lxcconn_fd , buffer, 1024,0);
         printf("%s\n",buffer );
         system(buffer);
     }
+    close(lxcsock_fd); //Close the lxc server socket
     return 0;
 }
 #endif
